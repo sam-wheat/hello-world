71daada8f963e719ab45e227798f6baca75b1355
sam-wheat <sam.wheat@outlook.com>
2017-01-25 17:47:16 -0800

    Add Timer

--- WebApplication1/src/WebApplication1/Program.cs
+++ WebApplication1/src/WebApplication1/Program.cs
@@ -4,20 +4,13 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using System.Threading;
-using System.Net.Http;
 
 namespace WebApplication1
 {
     public class Program
     {
-        private static Timer KeepAliveTimer;
-
-
         public static void Main(string[] args)
         {
-            KeepAliveTimer = new Timer(x => WakeUpAPI(), null, 0, 5000);
-            
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
@@ -26,19 +19,6 @@ namespace WebApplication1
                 .Build();
 
             host.Run();
-            
-        }
-
-
-        private static void WakeUpAPI()
-        {
-            HttpClient client = new HttpClient();
-            string dummy = client.GetStringAsync(new Uri("http://www.samwheat.com/api/api/blog/GetContentItemBySlug?slug=x&siteID=9999")).Result;
-        }
-
-        ~Program()
-        {
-            KeepAliveTimer.Dispose();
         }
     }
 }
--- WebApplication1/src/WebApplication1/Startup.cs
+++ WebApplication1/src/WebApplication1/Startup.cs
@@ -7,11 +7,16 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Net.Http;
 
 namespace WebApplication1
 {
     public class Startup
     {
+        private Timer KeepAliveTimer;
+        private HttpClient client = new HttpClient();
+
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -20,6 +25,8 @@ namespace WebApplication1
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
+
+            KeepAliveTimer = new Timer(x => CallAPI(), null, 0, 5000);
         }
 
         public IConfigurationRoot Configuration { get; }
@@ -32,7 +39,7 @@ namespace WebApplication1
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime appLifetime)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -53,6 +60,28 @@ namespace WebApplication1
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            
+            appLifetime.ApplicationStopping.Register(() => {
+                KeepAliveTimer.Dispose();
+            });
+
+            appLifetime.ApplicationStopped.Register(() => {
+                KeepAliveTimer.Dispose();
+            });
+        }
+
+
+        private void CallAPI()
+        {
+
+            try
+            {
+                string dummy = client.GetStringAsync(new Uri("http://www.samwheat.com/api/api/blog/GetContentItemBySlug?slug=Hello-World&siteID=12")).Result;
+            }
+            catch (Exception)
+            {
+                // dont care
+            }
         }
     }
 }
